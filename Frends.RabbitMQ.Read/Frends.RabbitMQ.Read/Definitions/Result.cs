4cbd34ede0855b58bb93b712bc0d94c30cf4d237
Teemu Tossavainen <teemu.tossavainen@hiq.fi>
2022-10-12 10:34:50 +0300

    Result.Success add

--- Frends.RabbitMQ.Read/Frends.RabbitMQ.Read/Definitions/Result.cs
+++ Frends.RabbitMQ.Read/Frends.RabbitMQ.Read/Definitions/Result.cs
@@ -8,6 +8,11 @@ namespace Frends.RabbitMQ.Read.Definitions;
 public class Result
 {
     /// <summary>
+    /// Read status. There was no messages to read if MessageUTF8 and MessagesBase64 are empty but Success=true.
+    /// </summary>
+    public bool Success { get; set; }
+
+    /// <summary>
     /// Message in Base64 format.
     /// </summary>
     /// <example>VGVzdCBtZXNzYWdl, {foo, bar}, 1, 1</example>
@@ -19,8 +24,9 @@ public class Result
     /// <example>Test message, {foo, bar}, 1, 1</example>
     public List<Message> MessageUTF8 { get; private set; } = new List<Message>();
 
-    internal Result(List<Message> messagesBase64, List<Message> messageUTF8)
+    internal Result(bool success, List<Message> messagesBase64, List<Message> messageUTF8)
     {
+        Success = success;
         MessagesBase64 = messagesBase64;
         MessageUTF8 = messageUTF8;
     }
